a3923b167258425ea74de0abe9d17db18ffba079
rdyzakya <randomzakya@gmail.com>
2024-11-14 23:54:53 +0700

    add gpi and value iteration

--- finite_markov_decision_process/grid_world_value_iteration.py
+++ finite_markov_decision_process/grid_world_value_iteration.py
@@ -1,7 +1,8 @@
 import numpy as np
 from tqdm import tqdm
-from typing import Tuple
+from typing import Tuple, Dict
 from enum import Enum
+from copy import deepcopy
 
 """
 Grid World problem
@@ -120,13 +121,62 @@ class Agent:
             state_values[state[0], state[1]] = v_s
         return state_values
     
-    def policy_evaluation(self, env : Environment, iterations : int=1000, epsilon : float=1e-5):
-        for _ in tqdm(range(iterations)):
-            new_state_values = self.sweep(env, self.state_values.copy())
-            l2 = np.sqrt(((new_state_values - self.state_values)**2).mean())
+    def policy_improvement(self, env : Environment, state_action_values : Dict):
+        states = env.get_all_states()
+        for state in states:
+            if env.is_terminal(state):
+                continue
+
+            for a, _ in self.policy(state).items():
+                q_pi = 0.0
+                dynamics = env.dynamics(state, a)
+                for new_state, rewards in dynamics.items():
+                    for reward, p in rewards.items():
+                        v_s_prime = self.state_values[new_state[0], new_state[1]]
+                        q_pi += p * (reward + self.discount_factor*v_s_prime)
+                state_action_values[state][a] = q_pi
+        return state_action_values
+    
+    def policy_evaluation(self, 
+                          env : Environment, 
+                          state_values : np.ndarray, 
+                          iterations : int=1000, 
+                          epsilon : float=1e-5):
+        for _ in tqdm(range(iterations), desc="Policy Evaluation"):
+            new_state_values = self.sweep(env, state_values.copy())
+            l2 = np.sqrt(((new_state_values - state_values)**2).mean())
             if l2 <= epsilon:
                 break
+            state_values = new_state_values
+        return state_values
+    
+    def generalized_policy_iteration(self, 
+                                     env : Environment, 
+                                     iterations : int=1000, 
+                                     state_values_epsilon : float=1e-5,
+                                     state_action_values_epsilon : float=1e-5,
+                                     evaluation_iterations : int=1000,
+                                     evaluation_epsilon : float=1e-5):
+        for _ in tqdm(range(iterations), desc="GPI"):
+            new_state_values = self.policy_evaluation(env, self.state_values.copy(), evaluation_iterations, evaluation_epsilon)
+            new_state_action_values = self.policy_improvement(env, deepcopy(self.state_action_values))
+
+            l2_state_values = ((new_state_values - self.state_values)**2).mean()
+            l2_state_action_values = 0.0
+
+            n = 0
+
+            for s, av in self.state_action_values.items():
+                for a, v in av.items():
+                    l2_state_action_values += (new_state_action_values[s][a] - v)**2
+                    n += 1
+            l2_state_action_values = np.sqrt(l2_state_action_values/n)
+
+            if l2_state_values <= state_values_epsilon and l2_state_action_values <= state_action_values_epsilon:
+                break
+
             self.state_values = new_state_values
+            self.state_action_values = new_state_action_values
 
 if __name__ == "__main__":
     N = 3
@@ -140,8 +190,14 @@ if __name__ == "__main__":
     print("Initial state values:")
     print(agent.state_values)
 
-    print("Policy evaluation...")
-    agent.policy_evaluation(env)
+    print("Initial state action values:")
+    print(agent.state_action_values)
+
+    print("GPI...")
+    agent.generalized_policy_iteration(env, evaluation_iterations=1) # Change to value iteration by making the evaluation iteration to 1
 
     print("Result state values:")
     print(agent.state_values)
+
+    print("Result state action values:")
+    print(agent.state_action_values)
