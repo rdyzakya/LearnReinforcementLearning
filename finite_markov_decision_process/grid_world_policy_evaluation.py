3d16a4883f5f74cf620a590636177c60b3595d62
randy-autentika-admin <Randy@autentika.id>
2024-11-14 18:23:11 +0700

    refactor

--- finite_markov_decision_process/grid_world_policy_evaluation.py
+++ finite_markov_decision_process/grid_world_policy_evaluation.py
@@ -1,6 +1,6 @@
 import numpy as np
 from tqdm import tqdm
-from typing import Tuple, List
+from typing import Tuple
 from enum import Enum
 
 """
@@ -16,10 +16,19 @@ class Action(Enum):
 class Environment:
     def __init__(self, n):
         self.world_size = (n, n)
-        self.terminal_states = [(0, 0), (self.world_size[0]-1, self.world_size[1]-1)]
+        self.terminal_states = [(0,0), (n-1, n-1)]
     
     def is_terminal(self, state : Tuple[int]):
         return state in self.terminal_states
+
+    def get_all_states(self):
+        states = []
+        for i in range(self.world_size[0]):
+            for j in range(self.world_size[1]):
+                states.append(
+                    (i,j)
+                )
+        return states
     
     def get_reward(self, state : Tuple[int], action : int, new_state : Tuple[int]): # r(s,a,s')
         if self.is_terminal(new_state):
@@ -38,23 +47,28 @@ class Environment:
             y = max(y - 1, 0)
         new_state = (x, y)
         reward = self.get_reward(state, action, new_state)
-        return new_state, reward
+        return {
+            new_state : {
+                reward : 1.0 # the probability is 1.0 because we make it deterministic
+            }
+        }
 
 class Agent:
     def __init__(self, env : Environment, discount_factor : int=0.5):
-        self.state_values = self.init_state_values(env) # V(s)
+        self.state_values = self.init_state_values(env)
+        self.state_action_values = self.init_state_action_values(env)
         self.current_state = (
             np.random.randint(low=0, high=env.world_size[0]),
             np.random.randint(low=0, high=env.world_size[1])
         )
         self.discount_factor = discount_factor
     
-    def init_state_values(self, env : Environment):
+    def init_state_values(self, env : Environment): # V(s)
         state_values = np.random.rand(*env.world_size)
         for terminal_state in env.terminal_states:
             state_values[*terminal_state] = 0.0
         return state_values
-    
+
     def available_actions(self, state : Tuple[int], env : Environment):
         actions = [a for a in Action]
 
@@ -70,32 +84,40 @@ class Agent:
         
         return actions
 
-    def get_policy(self, state : Tuple[int], actions : List[int]): # π(a|s), but for now it is deterministic. So, we ignore the state
-        return [1/(len(actions)) for _ in actions]
+    def init_state_action_values(self, env : Environment): # q_π(s,a)
+        state_action_values = {}
+        states = env.get_all_states()
+        for s in states:
+            available_actions = self.available_actions(s, env)
+            state_action_values[s] = {
+                a : 0.0 for a in available_actions
+            }
+        return state_action_values
     
-    def action(self, state : Tuple[int], env : Environment):
-        actions = self.available_actions(state, env)
-        pi = self.get_policy(state, actions)
-        action = np.random.choice(actions, p=pi)
-        return action
+    def policy(self, state : Tuple[int]): # π(a|s)
+        action_values  = self.state_action_values[state]
+        exp_action_values = {a : np.exp(v) for a, v in action_values.items()}
+        action_prob = {a : e/sum(exp_action_values.values()) for a, e in exp_action_values.items()} # we want to use softmax
+        return action_prob
     
     def sweep(self, env : Environment, state_values : np.ndarray):
-        for i in range(state_values.shape[0]):
-            for j in range(state_values.shape[1]):
-                state = (i, j)
-                if env.is_terminal(state):
-                    continue
+        states = env.get_all_states()
+        for state in states:
+            if env.is_terminal(state):
+                continue
 
-                actions = self.available_actions(state, env)
-                pi = self.get_policy(state, actions)
+            v_s = 0.0
 
-                v_s = 0.0
-                for a, prob in zip(actions, pi):
-                    new_state, reward = env.dynamics(state, action=a)
-                    v_s_prime = state_values[*new_state]
-                    v_s += prob * (reward + self.discount_factor*v_s_prime) # V(s) = Σ π(a|s) Σ p(s',r|s,a)[r + 𝛾V(s')] , since the reward and the next state is deterministic, the calculation is simple
+            for a, pi in self.policy(state).items():
+                q_pi = 0.0
+                dynamics = env.dynamics(state, a)
+                for new_state, rewards in dynamics.items():
+                    for reward, p in rewards.items():
+                        v_s_prime = state_values[*new_state]
+                        q_pi += p * (reward + self.discount_factor*v_s_prime) # q_π(s,a) = Σ p(s',r|s,a)[r + 𝛾V(s')]
+                v_s += pi * q_pi # V(s) = Σ π(a|s)*q_π(s,a)
                 
-                state_values[*state] = v_s
+            state_values[*state] = v_s
         return state_values
     
     def policy_evaluation(self, env : Environment, iterations : int=1000, epsilon : float=1e-5):
@@ -111,6 +133,9 @@ if __name__ == "__main__":
     DISCOUNT_FACTOR = 0.5
     env = Environment(N)
     agent = Agent(env, DISCOUNT_FACTOR)
+
+    print("Terminal states:")
+    print(env.terminal_states)
 
     print("Initial state values:")
     print(agent.state_values)
